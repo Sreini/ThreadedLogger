f21c057af1f551e09c333c19cf212ac38d58c7a9
Eirini Galati <eirini.galati@gmail.com>
2018-11-07 21:29:58 +0100

    wtf

---
+++ ThreadedLogger/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadedLogger
+{
+    public class Logger : ILogger
+    {
+        private ConcurrentQueue<Action> _queue = new ConcurrentQueue<Action>();
+        private Thread _loggerThread;
+        private Logger()    
+        {
+            _loggerThread = new Thread(ProcessQueue)
+            {
+                IsBackground = true,
+            };
+            _loggerThread.Start();
+        }
+
+        void ProcessQueue()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
