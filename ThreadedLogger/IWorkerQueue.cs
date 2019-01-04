e12f5d9b5639d17af435f535f8cf3a0fdfc39cc8
Eirini Galati <eirini.galati@gmail.com>
2019-01-04 15:41:40 +0100

    made the library more general and reusable, added constructor overloads to support configurable timeouts and configurable queue size

---
+++ ThreadedLogger/IWorkerQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WorkerQueue
+{
+    /// <summary>
+    /// Class that instantiates a queue in a different thread and performs any jobs pushed to the queue in that thread.
+    /// </summary>
+    public interface IWorkerQueue: IDisposable
+    {
+        /// <summary>
+        /// Flushes the queue. Useful for making sure the queue is empty, for example during error handling
+        /// </summary>
+        void Flush();
+
+        /// <summary>
+        /// Disposes the queue
+        /// </summary>
+        new void Dispose();
+
+        /// <summary>
+        /// Adds a job to the queue. The job should be performed in a parallel thread
+        /// </summary>
+        /// <param name="action">The job added to the thread</param>
+        /// <exception cref="ObjectDisposedException">Will be thrown if this method is called after the Dispose method has been called</exception>
+        void AddJob(Action action);
+
+
+    }
+}
---
+++ ThreadedLogger/WorkerQueue.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace WorkerQueue
+{
+    /// <inheritdoc/>
+    public class WorkerQueue : IWorkerQueue
+    {
+        private const int DefaultCollectionUpperBound = 100000;
+        private const int DefaultTimeout = -1;
+
+        private readonly BlockingCollection<Action> _queue;
+        private readonly Thread _workerThread;
+
+        private readonly ManualResetEvent _hasNewItems = new ManualResetEvent(false);
+        private readonly ManualResetEvent _endProcess = new ManualResetEvent(false);
+        private readonly ManualResetEvent _waiting = new ManualResetEvent(false);
+
+        /// <summary>
+        /// constructor with cancellation token and timeout
+        /// </summary>
+        /// <param name="collectionUpperBound">upper bound amount of items in queue</param>
+        /// <param name="timeout">time in milliseconds during which the process will try to take an item from the queue, or -1 for this time being indefinite</param>
+        public WorkerQueue(int timeout = DefaultTimeout, int collectionUpperBound = DefaultCollectionUpperBound)
+        {
+            _queue = new BlockingCollection<Action>(new ConcurrentQueue<Action>(), collectionUpperBound);
+
+            _workerThread = new Thread(ProcessQueue)
+            {
+                IsBackground = true,
+            };
+            object parameters = timeout;
+            _workerThread.Start(parameters);
+        }
+
+        /// <summary>
+        /// processes jobs on the queue
+        /// </summary>
+        private void ProcessQueue(object parameters)
+        {
+            var timeout = (int) parameters;
+            DoWork(timeout);
+        }
+
+        /// <summary>
+        /// main loop for processing jobs. 
+        /// </summary>
+        /// <param name="timeout"></param>
+        private void DoWork(int timeout)
+        {
+            while (true)
+            {
+                _waiting.Set();
+
+                //thread waits here until either _hasNewItems or _endProcess are signaled
+                var waitHandler = WaitHandle.WaitAny(new WaitHandle[] { _hasNewItems, _endProcess });
+
+                //_endProcess was signaled
+                if (waitHandler == 1)
+                {
+                    return;
+                }
+
+                _hasNewItems.Reset();
+                _waiting.Reset();
+                while (_queue.Count != 0)
+                {
+                    if(_queue.TryTake(out var action, timeout))
+                    {
+                        action();
+                    }
+                }
+            }
+        }
+
+        /// <inheritdoc cref="IWorkerQueue.Flush"/>
+        public void Flush()
+        {
+            _waiting.WaitOne();
+        }
+
+        /// <inheritdoc cref="IWorkerQueue.Dispose"/>
+        public void Dispose()
+        {
+            _endProcess.Set();
+            _workerThread.Join();
+            _queue.Dispose();
+        }
+
+        /// <inheritdoc cref="IWorkerQueue.AddJob"/>
+        public void AddJob(Action action)
+        {
+            if (_queue.IsAddingCompleted) return;
+            _queue.Add(action);
+            _hasNewItems.Set();
+        }
+    }
+}
